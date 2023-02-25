4635cddabc7a7b8cfab04546c43b467c2cd910f0
Bear Crump <ericbearcrump@gmail.com>
2023-02-24 23:29:24 -0600

    Timestamp 40:00

---
+++ Client/Services/SuperHeroService/SuperHeroService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorFullStackCrud.Client.Services.SuperHeroService
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient http;
+
+        public SuperHeroService(HttpClient http)
+        {
+            this.http = http;
+        }
+        public List<SuperHero> Heroes { get; set; } = new List<SuperHero>();
+        public List<Comic> Comics { get; set; } = new List<Comic>();
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SuperHero> GetSuperHero(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetSuperHeroes()
+        {
+            var result = await this.http.GetFromJsonAsync<List<SuperHero>>("api/SuperHero");
+
+            if (result != null)
+            {
+                Heroes = result;
+            }
+        }
+    }
+}
